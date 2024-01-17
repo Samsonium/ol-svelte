3cab7fc1f36f89e67421a9b06caaa492a96d0c7a
Samsonium <tdesu@vk.com>
2024-01-17 16:45:03 +0300

    Implement example usage

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Map from './lib/Map.svelte'
+    import Style from './lib/Style.svelte'
+    import LineString from './lib/LineString.svelte'
+    import VectorLayer from './lib/VectorLayer.svelte'
+</script>
+
+<main>
+    <Map>
+        <VectorLayer>
+            <LineString coordinates={[[0, 0], [1000000, 0], [1000000, 1000000], [0, 1000000], [0, 0]]} />
+            <Style stroke={{width: 4, color: 'red'}} />
+        </VectorLayer>
+    </Map>
+</main>
+
+<style>
+  main {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+</style>
