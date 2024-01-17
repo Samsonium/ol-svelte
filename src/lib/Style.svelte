a9921a25248e74dca13f14231dc391b58c883268
Samsonium <tdesu@vk.com>
2024-01-17 16:44:54 +0300

    Implement Style component

---
+++ src/lib/Style.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {getContext, onMount} from 'svelte'
+    import {Fill, Stroke, Style} from 'ol/style.js'
+
+    /**
+     * @type {{
+     *     width: number
+     *     color: string
+     * }}
+     */
+    export let stroke
+
+    /**
+     * @type {{color: string}}
+     */
+    export let fill
+
+    /** @type {{layer: import('ol/layer').Vector}} */
+    const {layer} = getContext('ol-svelte-vector')
+    onMount(() => {
+        if (!layer) return;
+
+        layer.setStyle(new Style({
+            fill: fill ? new Fill(fill) : undefined,
+            stroke: stroke ? new Stroke(stroke) : undefined
+        }));
+    })
+</script>
