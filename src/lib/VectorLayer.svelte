5e2cadd75c6cc3f610d997a2a4b5ef9962874d7f
Samsonium <tdesu@vk.com>
2024-01-17 16:44:48 +0300

    Implement VectorLayer component

---
+++ src/lib/VectorLayer.svelte
@@ -0,0 +1,21 @@
+<script>
+    import {getContext, onMount, setContext} from 'svelte'
+    import VectorLayer from 'ol/layer/Vector'
+    import VectorSource from 'ol/source/Vector'
+
+    const source = new VectorSource()
+    const layer = new VectorLayer({source})
+
+    /** @type {{map: import('ol').Map}} */
+    const {map} = getContext('ol-svelte');
+    onMount(() => {
+        if (!map) return;
+        map.addLayer(layer);
+
+        () => layer.dispose();
+    })
+
+    setContext('ol-svelte-vector', {map, layer, source});
+</script>
+
+<slot/>
